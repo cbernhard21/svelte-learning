3163ef28440e9d34bd999320f16ac31ff9e0f786
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-04-07 18:32:01 +0000

    started some card components

---
+++ src/components/PopularMovies.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let popular;
+    import MovieCard from './MovieCard.svelte'
+</script>
+
+<h3>Popular Movies</h3>
+
+<div class="popular-movies">
+    <div class="movie">
+        {#each popular as movie}
+            <MovieCard {movie}/>
+        {/each}
+    </div>
+</div>
+
+<style>
+    h3 {
+        padding: 0 1rem;
+    }
+
+    .popular-movies {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        column-gap: 1rem;
+        row-gap: 2rem;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export async function load({fetch}){
+        const res = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=ba119c72f06e64c08f7e104f49c1699e&language=en-US&page=1`
+        );
+        const data = await res.json();
+        // console.log(data.results)
+        if(res.ok){
+            return{
+                props: {popular: data.results}
+            }
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte";
+    export let popular;
+</script>
+
+<section>
+    <PopularMovies {popular} />
+</section>
+
+
+
+
